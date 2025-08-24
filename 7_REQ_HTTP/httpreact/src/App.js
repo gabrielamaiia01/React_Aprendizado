9792ac1d1c323206d85c49a98d08d504c60dbd2c
gabrielamaiia01 <gabrielamaiia01@gmail.com>
2025-08-24 15:22:42 -0300

    Carregamento de dados API

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -6,6 +6,9 @@ function App() {
 
   const url = "http://localhost:3000/products"; 
   const [produtos, setProdutos] = useState([])//salva esses produtos
+  
+  const [name, setName] = useState(""); 
+  const [price, setPrice] = useState(""); 
 
   //1 - resgastando dados
   useEffect(() => { 
@@ -18,11 +21,67 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(produtos)
+
+  //Adição de Produtos
+
+  //Função que envia o formulário 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name, 
+      price, 
+    };
+
+    const res = await fetch(url, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify(product)
+    })
+
+     //3 - carregamento dinamico 
+const addedProduct = await res.json()
+
+setProdutos((prevProdutos) => [...prevProdutos, addedProduct])
+
+setName("")
+setPrice("")
+  }
+
+
+ 
 
   return (
     <div className="App">
     <h1>Lista de produtos</h1>
+    <ul> 
+      {produtos.map((produto) => (
+        <li key={produto.id}>{produto.name} - R${produto.price} </li>
+      ))}
+    </ul>
+
+    <div className='add-product'> 
+
+      <form onSubmit={handleSubmit}> 
+        <label>
+          nome: 
+          <input type='text' 
+          value={name} 
+          name='name' 
+          onChange={(e)=> setName(e.target.value)}/>
+        </label>
+        <label>
+          preço:
+          <input type='number' 
+          value={price} 
+          name='price' 
+          onChange={(e)=> setPrice(e.target.value)}/>
+        </label>
+        <input type='submit' value='Criar'/>
+      </form>
+    </div>
     </div>
   );
 }
